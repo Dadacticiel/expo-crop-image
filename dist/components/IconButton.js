1bfeac38b6d7ba1677346dd8558a055afb81d526
brendan.seveno <brendan.seveno@additi.fr>
2023-11-07 17:23:08 +0100

    Update

--- dist/components/IconButton.js
+++ dist/components/IconButton.js
@@ -1,7 +1,10 @@
-import { TouchableOpacity } from 'react-native';
-import { Icon } from './Icon';
+import { TouchableOpacity } from 'react-native'
+import { Icon } from './Icon'
 export function IconButton({ text, iconID, color, ...buttonProps }) {
-    return (<TouchableOpacity {...buttonProps}>
-      <Icon text={text} iconID={iconID} color={color}/>
-    </TouchableOpacity>);
+  return (
+    <TouchableOpacity {...buttonProps}>
+      <Icon text={text} iconID={iconID} color={color} />
+    </TouchableOpacity>
+  )
 }
+//# sourceMappingURL=IconButton.js.map
--- dist/context/editor.js
+++ dist/context/editor.js
@@ -1,2 +1,3 @@
-import { createContext } from 'react';
-export const EditorContext = createContext({});
+import { createContext } from 'react'
+export const EditorContext = createContext({})
+//# sourceMappingURL=editor.js.map
